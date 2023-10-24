c00d6fff1eaae4b49c9a2e4c37defd15d234fdb7
likhith123-code <likhith2901@gmail.com>
2023-10-24 10:53:01 +0530

    port corrected

--- app.js
+++ app.js
@@ -52,12 +52,12 @@ class App {
     }
 
     async listen() {
-        this.app.listen(4200, (err) => {
+        this.app.listen(3000, (err) => {
             if (err) {
                 console.log("Error while running the express application", err);
             }
             else {
-                console.log("Express application running on port 4200");
+                console.log("Express application running on port 3000");
             }
         })
     }
