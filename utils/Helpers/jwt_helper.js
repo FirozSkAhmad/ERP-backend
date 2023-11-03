7b6189b1fa19d6cda9e1652627e27dff86dd61fc
Likhith Kumar <55198695+likhith123-code@users.noreply.github.com>
2023-11-03 15:33:36 +0530

    Access token expiry time increased

--- utils/Helpers/jwt_helper.js
+++ utils/Helpers/jwt_helper.js
@@ -9,7 +9,7 @@ class JWTHelper {
             const payload = {}
             const secret = process.env.ACCESS_TOKEN_SECRETKEY
             const options = {
-                expiresIn: '10m',
+                expiresIn: '1y',
                 issuer: 'vrcapplication',
                 audience: tokenPayload,
             }
